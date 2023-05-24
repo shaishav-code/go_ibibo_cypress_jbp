bdfd25958151d2abbff1597f6c8ee24e7e159748
jaypatel999 <jay@loadstop.com>
2023-05-24 15:54:34 +0530

    Cypress Git Integration

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      /// <reference types=”cypress-xpath” />
+    },
+
+    testIsolation: false,
+    projectId: "r9v9pq",
+  },
+});
---
+++ cypress/e2e/bookmyshow.cy.js
@@ -0,0 +1,64 @@
+/// <reference types="Cypress" />
+
+import DatePicker from "../pageobjects/DatePicker"
+
+
+
+context('Hotel booking', () => {
+  it('Visited Goibibo website', () => 
+  {
+    cy.visit('https://www.goibibo.com/')
+
+  })
+
+
+  it('Select location', () => {  
+    cy.get(':nth-child(2) > .nav-link').click()
+    
+    cy.get('#downshift-1-input').type('Surat')
+    cy.get('#downshift-1-item-0').click()
+  })
+
+  it('Select the date', () => {
+    
+    cy.get('[data-testid="openCheckinCalendar"]').click()
+
+
+    
+    cy.get('[data-testid="date_23_5_2023"]').click()
+    cy.get('[data-testid="date_25_5_2023"]').click()
+
+    cy.get('.SearchBlockUIstyles__CitySearchInput-sc-fity7j-13').click()
+    cy.get('[data-testid="button-adult-dec"]').click()
+    cy.get('[data-testid="button-adult-add"]').click()
+    cy.get('.PaxWidgetstyles__ButtonWrapperDiv-sc-gv3w6r-10 > .dwebCommonstyles__ButtonBase-sc-112ty3f-13').click()
+    cy.get('[data-testid="searchHotelBtn"]').click()
+    // cy.get('.close').click()
+
+  })
+
+  it('Compare hotels', () => {
+
+    cy.get(':nth-child(1) > .HotelCardstyles__OuterWrapperDiv-sc-1s80tyk-1 > .HotelCardstyles__WrapperSectionMetaDiv-sc-1s80tyk-8 > .HotelCardstyles__HotelCardInfoWrapperDiv-sc-1s80tyk-14 > .HotelCardstyles__HotelPricingInfoWrapperDiv-sc-1s80tyk-29 > :nth-child(2) > :nth-child(1) > .HotelCardstyles__PricingCompleteSection-sc-1s80tyk-69 > .HotelCardstyles__CurrentPriceTextWrapper-sc-1s80tyk-39 > .HotelCardstyles__CurrentPrice-sc-1s80tyk-40').invoke('text').then((text1) => {
+      const price1 = parseFloat(text1.replace('₹', ''));
+
+      cy.get(':nth-child(2) > .HotelCardstyles__OuterWrapperDiv-sc-1s80tyk-1 > .HotelCardstyles__WrapperSectionMetaDiv-sc-1s80tyk-8 > .HotelCardstyles__HotelCardInfoWrapperDiv-sc-1s80tyk-14 > .HotelCardstyles__HotelPricingInfoWrapperDiv-sc-1s80tyk-29 > :nth-child(2) > :nth-child(1) > .HotelCardstyles__PricingCompleteSection-sc-1s80tyk-69 > .HotelCardstyles__CurrentPriceTextWrapper-sc-1s80tyk-39 > .HotelCardstyles__CurrentPrice-sc-1s80tyk-40').invoke('text').then((text2) => {
+        const price2 = parseFloat(text2.replace('₹', ''));  
+
+        if (price1 > price2)
+        {
+          cy.get(':nth-child(2) > .HotelCardstyles__OuterWrapperDiv-sc-1s80tyk-1 > .HotelCardstyles__WrapperSectionMetaDiv-sc-1s80tyk-8 > .HotelCardstyles__HotelCardInfoWrapperDiv-sc-1s80tyk-14 > .HotelCardstyles__HotelPricingInfoWrapperDiv-sc-1s80tyk-29 > :nth-child(2) > :nth-child(1) > .HotelCardstyles__PricingCompleteSection-sc-1s80tyk-69 > .HotelCardstyles__CurrentPriceTextWrapper-sc-1s80tyk-39 > .HotelCardstyles__CurrentPrice-sc-1s80tyk-40').click()
+        }
+        else {
+          cy.get(':nth-child(2) > .HotelCardstyles__OuterWrapperDiv-sc-1s80tyk-1 > .HotelCardstyles__WrapperSectionMetaDiv-sc-1s80tyk-8 > .HotelCardstyles__HotelCardInfoWrapperDiv-sc-1s80tyk-14 > .HotelCardstyles__HotelPricingInfoWrapperDiv-sc-1s80tyk-29 > :nth-child(2) > :nth-child(1) > .HotelCardstyles__PricingCompleteSection-sc-1s80tyk-69 > .HotelCardstyles__CurrentPriceTextWrapper-sc-1s80tyk-39 > .HotelCardstyles__CurrentPrice-sc-1s80tyk-40').click()
+        }
+        })
+    })
+  })
+
+  })
+  
+
+
+
+  
